097ca88b47b670b1f564489d6fa383606181f49f
Larry (Omega-X) <castro_larry@yahoo.com>
2025-01-29 18:47:13 +0000

    Few modifications to support role setting

--- get_ids.py
+++ get_ids.py
@@ -116,63 +116,9 @@ class GetRefIds:
 
         # self.get_ref_ids(each_line)
         parsed_names = self.file.parser.parse_line(each_line, self.x500list)
-        for each_name in parsed_names:
-            each_name.identify_party_role(each_line)
+
+        # for each_name in parsed_names:
+        #     each_name.identify_party_role(each_line)
+        # self.file.identify_party_role(each_line)
 
         return parsed_names
-
-#
-# if __name__ == "__main__":
-#     log_file = None
-#     app_path = os.path.dirname(os.path.abspath(__file__))
-#     app_path_support = app_path
-#     logfile_format = None
-#     parserargs = argparse.ArgumentParser()
-#     parserargs.add_argument('-l', '--log-file',
-#                             help='Give actual log file to pre-format')
-#
-#     args = parserargs.parse_args()
-#     # load_corda_object_definition('%s/conf/logwatcher_rules.json' % (app_path,))
-#     Configs.load_config()
-#     load_rules()
-#
-#     alternate_name_found = False
-#     # x500_list = []
-#     test_list = []
-#     # in this case I'm removing initial branch 'UML_SETUP' because final config is a collection of configuration settings
-#     # that removes this.
-#     rules = Configs.get_config_for('UML_DEFINITIONS.participant')
-#     parser = X500NameParser(rules)
-#
-#     if not args.log_file:
-#         print('You must provide a log file to scan')
-#         exit(0)
-#     else:
-#         log_file = args.log_file
-#
-#     if log_file:
-#         with open(log_file, "r") as fh_log_file:
-#             for each_line in fh_log_file:
-#                co = get_ref_ids(each_line)
-#                parsed_names = parser.parse_line(each_line, test_list)
-#
-#             print("-------------\nParsed X500 Names:")
-#
-#             for each_name in parsed_names:
-#                 print(f"* {each_name.string()}")
-#                 if each_name.has_alternate_names():
-#                     for each_alternatename in each_name.get_alternate_names():
-#                         print(f'   Alternate Name: {each_alternatename}')
-#
-#         if CordaObject.list:
-#             print('-------------')
-#             for each_type in CordaObject.list:
-#                 print(f'* Found {len(CordaObject.list[each_type])} {each_type}(S)')
-#
-#             saving_tracing_ref_data(CordaObject.get_all_objects(), log_file=args.log_file)
-#
-#             trace_id(args.log_file)
-#
-#     pass
-#
-#
