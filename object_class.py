c991cd4de14d457355dd2e9958bd1d0a73f7b5c4
Larry (Omega-X) <castro_larry@yahoo.com>
2025-02-26 12:31:23 +0000

    Adding a New method to search on Tuples and list a non null value

--- object_class.py
+++ object_class.py
@@ -3530,6 +3530,24 @@ class RegexLib:
             return ltr
 
 
+def get_first_not_null(list_or_tuple, start=0):
+    """
+    Returns first not null object within a tuple or list
+    :param list_or_tuple: list or tuple object to check
+    :param start: it will indicate how this routine will start from, normally a list start in 0 but in some cases
+    (like sending tuple list from re.groups() this should start in 1
+    :return: int or None
+    """
+
+    try:
+        index = next(i for i, value in enumerate(list_or_tuple) if value is not None)
+        return index + start
+
+    except StopIteration:
+        return None
+
+
+
 def generate_internal_access(variable_dict, variable_to_get):
     """
     This method generates internal access to a given variable in a nested dictionary.
