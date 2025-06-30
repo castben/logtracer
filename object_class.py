8eaa0bfd02fc048d98f1fd061b5441b2a8893c83
Larry (Omega-X) <castro_larry@yahoo.com>
2025-06-30 07:59:44 +0100

    Handle references when two or more hit same line

--- object_class.py
+++ object_class.py
@@ -1413,7 +1413,20 @@ class FileManagement:
         if element_type not in FileManagement.unique_results:
             FileManagement.unique_results[element_type] = {}
 
-        FileManagement.unique_results[element_type].setdefault(item.reference_id, item)
+        if element_type == CordaObject.Type.UML_STEPS.value and  item.reference_id in FileManagement.unique_results[element_type]:
+            # This mean another item.reference_id is already there with same reference in this case line number, this
+            # could be because same line contains a transaction and a flow for example...
+            # let's check container is a list to be able to add it, otherwise I need to convert container into a list, add previous
+            # value, and allow new value to be added..., other wise, if it is already a list, add new value
+            #
+            if not isinstance(FileManagement.unique_results[element_type][item.reference_id], list):
+                previous_value =  FileManagement.unique_results[element_type][item.reference_id]
+                FileManagement.unique_results[element_type][item.reference_id] = []
+                FileManagement.unique_results[element_type][item.reference_id].append(previous_value)
+
+            FileManagement.unique_results[element_type][item.reference_id].append(item)
+        else:
+            FileManagement.unique_results[element_type].setdefault(item.reference_id, item)
 
     def assign_roles(self):
         """
