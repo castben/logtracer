b9fc7786e3a421ae71e5f741c3bef96ebfaf89b1
Larry (Omega-X) <castro_larry@yahoo.com>
2025-08-04 13:55:21 +0100

    Modifying base main window

--- logtracer.py
+++ logtracer.py
@@ -42,12 +42,12 @@ class InteractiveWindow:
                                                      flags=ttk.TTkK.WindowFlag.WindowMinMaxButtonsHint)
 
             self.root_logviewer = TTkUiLoader.loadDict(TTkUtil.base64_deflate_2_obj(
-                "eJyFU1tv0zAUTmnatCsXAWN0o5MqnvpULq9cHjYKiKxjYoE9IeQmpsdqYleJvbVIk3jspPNo/i/HSYsmXhYr8rn7+5wvv/0/uu6Vz6UdoK+Xc27xbhTNvopn71RsMi61" +
-                "xeCc54VQ0mLj5fDF8LnFujbCupZGnLKisHibeg6V1ExInltszlnOsqIs8Y9ZRlO3xpQ7EzJRFxZbJ6oQ2o38bgehH97i6J+KX7x05+E+x/ZYyP6ZSDTY0KNm8j5yMQXt" +
-                "3PaYLdbJT55Xc3kKrPNVJPgmCjFJuV1hMJKMrMSZkVJpJOYWPaJ1wpJEyGl5qFctjs0jtlSGWLeJ0to22EwriwhBAD1oXxKJD1xlXOfLdTvh1rbAVgwiTXLuuJXl2KJJ" +
-                "73N3C65vAB3c2UR+pGr6U6T8XPALuji447DUCAfcc1YW9jjcDz14QO/DkhlsV9ujFeys4DF0YbfqKReHJ9DD5oHKExq3wkYkNF0CdA3sX8MO/QryLNwjyPB0g7RDuCK+" +
-                "0KNE6H9gd68Fb8Arwr0b8WLniETSPzbZpIS4fc3tn2qWa/dNwhq2v3CW9D/LdElVW2OTatF3tXZlsJ6TjEgJ9Vilbg/IL+ZMur6AYmvbMIMBidgcsJwoYnOitFZZacak" +
-                "bQLgqG+YdstSQyXyf6IOuO9uwaI/Wginj0Pg8cwpy15hUDqksStT0A8Br4j/a3rfhDV4W+Iw2ImVlDx2si/oWDP8C6rONi8="))
+                "eJyFU0tv00AQdogTxw0PQUtpSypFnHIKjyuPQ0sA4aZUNNATqjb2NrOKvRvZu2mCVIljKs1x+b/MOgnKrV5ZO8/d7xt//uP/VVWvfG5sB309n3CLDweD8Q/x8qOKTcal" +
+                "thhMeV4IJS3W3nRfd19ZrGojrGupxSkrCov3qedYSc2E5LnF+oTlLCvKEv+UZXTqVp9yF0Im6tpi40wVQrsjf9lO5Ef3OPrn4jcv3Ul0yDHsC9m+EIkGG3nUTN4XLkag" +
+                "nRv22WyV/Op5FZenwCq/jAQ/RSGGKbcLDHqSkZU4c6BUOhATix7ROmNJIuSovNRbLo71EzZXhliHRGllG6ynS4sIQQAtCG+IxGeuMq7z+aqdcGtbYCMGkSY5d9zKcmzQ" +
+                "SZ9yNwXX14Em7q4jl6kaXYmUTwW/psHBA4elQjjgkbOyqMXhceTBE3q3S2aws9yeLmB3Ac9gD/aXPeXi8BxaWD9SeULHLbA2EJqGAHsGDjewQ3sJeRwdEGR4sUbaJFwD" +
+                "PtO9ROj/YPc3gnfgFdHBnXixeUIiaZ+abFhC3Nlw2+ea5dp9k6iC4XfOkvY3mc6paqtvUi3artYuDFZzkhEpoRqr1O0B+cWESdcXUGxlG2YwIBGbI5YTRawPldYqK82Y" +
+                "tE0AHPU10+2y1FCJvEyvppd85sbgMPtuABb9notgeAw8HjtR2VsMSofkdWsK+hfgLVF/R+/7qAIfSggGm7GSksdO8QXdaLr/AHinNBI="))
 
 
             self.TTkFrame_logfileviewer: ttk.TTkFrame = self.root_logviewer.getWidgetByName('TTkFrame_logfileviewer')
