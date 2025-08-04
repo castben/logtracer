5c5172c6257f34ba5fa5de374718f1e446860528
Larry (Omega-X) <castro_larry@yahoo.com>
2025-08-04 13:52:20 +0100

    Adding support to prevent unexpected crashes due to objects not found

--- get_refIds.py
+++ get_refIds.py
@@ -186,6 +186,12 @@ class GetRefIds:
         # TODO: He descubierto una discrepancia en los objetos "unicos" y los que reporta CordaObject.list
         # TODO: En este presente ejemplo hay 2 flows que no son tomados en cuenta en los resultados finales, pero si estan en CordaObject.list
         classified_results = {}
+
+        if not results:
+            write_log('Unable to get any references on this file, please try another file...', level='WARN')
+
+            return None
+
         for each_item in results:
             if each_item.type not in classified_results:
                 classified_results[each_item.type] = {}
