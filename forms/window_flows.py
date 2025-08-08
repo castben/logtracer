9526733d90ac43375b747721e6fd48bb38e299cf
Larry (Omega-X) <castro_larry@yahoo.com>
2025-08-08 07:30:45 +0100

    Few widgets modifications

--- forms/window_flows.py
+++ forms/window_flows.py
@@ -2,13 +2,13 @@ from TermTk import TTkUtil, TTkUiLoader, TTk
 
 # Data generated using ttkDesigner
 widget = TTkUiLoader.loadDict(TTkUtil.base64_deflate_2_obj(
-    "eJyNk1lv00AQgB3lcJIeKVdJRYEICdEqqBw/oU1bwKRqVdM+ocjYS2dVxxvsdQ+kSn1MpHlBWp554A/yE5j1OqEIQolleY6dmW9mJ5elrwtlK/tdqBUsyfMBUzjvusfv" +
-    "+LOO8NM+i6RC+4TFCReRwvLLtRdrzxUWZcqVDin7oZckCmcpZkNE0uMRixVWBl7s9ZPsSGnH61PWhS75DnkUiNPex1CcUlB1VyRc6sTv1YpTcgoMS/v8M8vUrvOQYa3L" +
-    "o9YhDyQox8K61l4xfgRSq7Wud5Y731hWQfvJkPuNxT7gCf8QMjVEezPySAq06AoRunyg0KLmdr0g4NFRVtQyD8PKW+9cpNR7jRrL5RQroZGoLbBhGWoX1MQ2E30m4/M8" +
-    "nLilSrDqAw+DmOnesuNYpUxbsZ6FjluBGZwbW7KBKJjTCAUqDw0ttZ0HDG44Ftyk91bWENw2nztDWBzCXWjCkonJHgb3YBkr6yIO6BKGWHa5pN6xvJXNO4X7V8ChZXhX" +
-    "nWXihUeUx3gfj/Fmr+LldNaEbtVZ+k+68VA1HbRH8BSa01CWchQzMVvPnidSXSXSht/nVciJnjiL1xLh/D4Lma+XrtUVAVNOAaudqJMrVorFmFLTdhV9EeqvTXoy8CJ9" +
-    "0iZbLqdeCh2qs0nvllOAbTIZar0x66mUtNZj7sbE1PNDkbAcfU6PMEOvO0WNbhN6cSp6yWVntJHlDZOiPcTaBjD/WK+1GqGdKbTgoylk8PovPDL2fDa53DHPrOGp/4sH" +
-    "drD+49uX7y3XpGgPYXcEe9dXb/6qru+x9ynl/vEJZ+MbbU4wGgajcQ1GgzAuW3s6TesgyzOdhf1p8tIUZ3wRRWYvEvrHpms/AbsJn7E="))
+    "eJyNk1tPE0EUgLfpZdtyKV4QUIyNiUkTSEV+Ai2griUQVngyZN0dORO2O3V3BqgJCY8lmReT8dkH/6A/wTMzW2wCiN1s9lzmnPnOpZelH3Nlx/wuVEuW+HBAlJz1/ZOP" +
+    "9HWXhaJPEq6ke0rSjLJEyfJ6+017TckiF1TpkHIYB1mm5DTGdFjCA5qQVMnKIEiDfmaOlHaCPmad66HvkCYROzv6ErMzDKrusoxynfiTanklr0BkaZ9+I0bteS+IrPVo" +
+    "0jykEQflObKutbeEHgPXaq0XnOfO945T0H405H5rcQ9oRj/HRI2ku5kEKEVa9BmLfTpQ0sHidoMoosmxudSxD5GVD8GQCay9hoXlspCV2EpYFriwDLULLGKbsD7h6TAP" +
+    "R26uMlkNgcZRSnRt5risYqatVPdCx7VgSs6MLaYhCmZyBAINLa17ywQeeA48xPeRKQge28/8CJ6MYAEWYWkSG57BsqxssDTCIVzJsk851g6LAp5PIEPTRmB+JIWXmMF6" +
+    "X43BpifBbnCteUv/yVWwj+GClRGsyvKWGf3tQG1vIQeyHXN172nG1SSXNkz2y+Q3XKve/L1ccnafxCTUS9fssYgoryCr3aSbK46QxRRT43YVQxbrr4t6NggSfdJFWy6L" +
+    "QEAX79nEd8srwDaaLLXemA3BOa71mLtxbToKY5aRHH1WN9Kg172iRncRvXgneskn57iR5Y5NsTKStQ6Q8ESvNU7bNQou+NUdZPDuFh6eBiG5HvGYZ9ry1P/FAzuy/vvn" +
+    "919N36bA+e5ewd79ty/+vV3P8eiroOHJKSXjiT69xmhYjMY9GA3EuGzu6TTNA5PnbhZy0xQIIadCliR2LzL8x4r2H3iGn88="))
 
 root=TTk()
 root.layout().addWidget(widget)
