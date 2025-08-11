f755cf0f076383ad827b5dee943586aad430720f
larrycastro <larry.castro@r3.com>
2025-08-11 09:54:49 +0100

    * Widget updates * Adding support to review special block details

---
+++ forms/frame_specialblocks_details.py
@@ -0,0 +1,15 @@
+from TermTk import TTkUtil, TTkUiLoader, TTk
+
+# Data generated using ttkDesignerx
+widget = TTkUiLoader.loadDict(TTkUtil.base64_deflate_2_obj(
+    "eJylVGtr01AYTu0lvehEt24rbhAEoaBWV2Q/YO6mWWdZo4OByGly2HtYmlOSE12FgR8zOB+PH/xP/ijfk6S1aMcEE0Lee57nOW/7rfT9Z8lIryvVliUxGVMllxzn4j17" +
+    "scvdeEQDoaT5mYYR44GS5W5nq/NSyaKImdItZdcnUaTkXex5zQNBWEBDJStjEpJRlJaUjskIp9Z7mDtlgce/KFnt84gJPfKjatsl+w6VpQH7SlP3h71JZa3HAuuUeQKU" +
+    "bWAzeoeUnYPQbq1HLvPkW8Mo6DwG8nwWMT+wiA19qhJp7gUELU+bDue+w8ZKGkirTzyPBefpR43sprJyRCY8RtY1pJTbsaz4mYWEwIQNqF0hiQPKR1SEk7wdcQsVyaoL" +
+    "zPdCqrml5bKKk/ZDrYLua0NDrk4jn6IxdRnxhz53LyIF9zSWIuoB97V1bbcoPLANeIjPcsoMVrJXM4HVBNZgHVrz+OERbMjKDg89PIdrWXaYQBFgPYbNOexgZR04HyHD" +
+    "Y5yQZZ9MEdanCJ9v5ai29fQU1Ym99o+oCtmdooKnCTyT5i7FJfEjtRhQ327mgDLpGgjDoZdiz2Nipl5rLrhQwMIM6pndvBWqbBzh1lrH8WiYarYy51oDQUKhl0Sv3Qkl" +
+    "nvUu8Ce4SvVe7Atm6VqVxLIY4l5jTdHlvn6b6EdjEii7IE2M5XZMYthHKAf4HNoFeIOhG5Xv5nSMGZ3t/1B+aZApZe1kUi0+gFd/HICptQ7p79Vt5oEbhC/kSLv28u3C" +
+    "mwOeiZv8rcoCoeiiqlg2XB4E1NV/JxH+5uLOL23Vi28="))
+
+root=TTk()
+root.layout().addWidget(widget)
+root.mainloop()
