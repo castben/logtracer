a1fdb96dc9ade120baa0b2452d3b5620269654c8
Larry (Omega-X) <castro_larry@yahoo.com>
2026-01-07 08:47:37 +0000

    Misc modification

--- test_api.py
+++ test_api.py
@@ -1,4 +1,4 @@
 import json
 from core import analyze_corda_log
-result = analyze_corda_log("client-logs/ChainThat/CS-4002/party005-dev-corda-logs.txt")
+result = analyze_corda_log("c4-logs/client-logs/ChainThat/CS-4002/party005-dev-corda-logs.log")
 print(json.dumps(result, indent=2))
