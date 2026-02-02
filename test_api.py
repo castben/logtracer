4294a613e195f7819231ba4d339ad093fb9f60e5
Larry (Omega-X) <castro_larry@yahoo.com>
2026-02-02 14:08:00 +0000

    Correcting Test

--- test_api.py
+++ test_api.py
@@ -1,6 +1,8 @@
 import json
 from core import analyze_corda_log
-#result = analyze_corda_log("c4-logs/client-logs/ChainThat/CS-4002/party005-dev-corda-logs.log")
-result = analyze_corda_log("client-logs/ChainThat/CS-4002/party005-dev-corda-logs.txt")
+from object_class import CordaObject
+
+result = analyze_corda_log("c4-logs/client-logs/ChainThat/CS-4002/party005-dev-corda-logs.log",CordaObject.Type.ERROR_ANALYSIS)
+#result = analyze_corda_log("client-logs/ChainThat/CS-4002/party005-dev-corda-logs.txt")
 
 print(json.dumps(result, indent=2))
