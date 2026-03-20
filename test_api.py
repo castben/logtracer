b0ec78a640636ef0eaedc1d31979c4b95b632033
Larry (Omega-X) <castro_larry@yahoo.com>
2026-03-20 08:25:30 +0000

    Adding support for Persistence, this will help to serialize data and save it into disk to be able to analyse it later.

--- test_api.py
+++ test_api.py
@@ -1,9 +1,73 @@
 import json
-from core import analyze_corda_log
+
+import drivers.yaml_driver
+from core import analyze_corda_log, DataInfo
+from drivers.yaml_driver import YamlDataDriver
 from object_class import CordaObject
 
-result = analyze_corda_log("c4-logs/client-logs/Nexi Payments/CS-4105_07-01-26/node-ascorda_Bank03069_01-full.log",
-                           CordaObject.Type.ERROR_ANALYSIS)
+# Tests:
+# pull a list of:
+# Transactions
+# Flows
+# Parties
+# Errors found
+# TODO: everytime it ran it "forgets" about what was analysed, this will prevent analysis of specific flows or tx
+#       I need to include a method to "save" what was collected so program has something to look at
+
+datainfo = DataInfo()
+datainfo.set(DataInfo.Attribute.CUSTOMER, "test-customer")
+datainfo.set(DataInfo.Attribute.TICKET, "TST-0000")
+
+#result = analyze_corda_log("/home/larry/IdeaProjects/logtracer/c4-logs/client-logs/NCR/CS-4189/2026-03-10-11-prd-api-service.log",
+result = analyze_corda_log("/home/larry/IdeaProjects/logtracer/c4-logs/tests-logs/insuree.log",
+                           [CordaObject.Type.ERROR_ANALYSIS, CordaObject.Type.FLOW_AND_TRANSACTIONS, CordaObject.Type.PARTY],
+                           datainfo)
 #result = analyze_corda_log("client-logs/ChainThat/CS-4002/party005-dev-corda-logs.txt")
+
+
+def save_analysis(result, object_type=None):
+    """
+    Store analysis.
+    :param object_type:
+    :return:
+    """
+
+    if not object_type:
+        object_type = [
+            CordaObject.Type.FLOW_AND_TRANSACTIONS,
+            CordaObject.Type.PARTY,
+            CordaObject.Type.ERROR_ANALYSIS,
+        ]
+
+    for each_object in object_type:
+        storage = YamlDataDriver()
+
+        customer = datainfo.get(DataInfo.Attribute.CUSTOMER)
+        ticket = datainfo.get(DataInfo.Attribute.TICKET)
+
+        if each_object == CordaObject.Type.ERROR_ANALYSIS:
+            config = {
+               'data_dir': f"./data/storage/{customer}/{ticket}",
+               'cache_enabled': True
+            }
+
+            storage.connect(config)
+
+            for each_item in result["result"][each_object]:
+
+
+
+
+
+
+
+
+
+    pass
+
+
+
+
+
 
 print(json.dumps(result, indent=2))
